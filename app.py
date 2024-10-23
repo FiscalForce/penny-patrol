011d4db91395fc22398066decafe6e299b1778a4
Arun TU <arun.tu@dc.cognicor.com>
2024-10-23 22:27:41 +0530

    base

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print("Hello World")
+from flask import Flask, render_template, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.secret_key = "12345"
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Arun@123' 
+app.config['MYSQL_DB'] = 'penny_patrol'
+
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+
+@app.route('/groups')
+def home():
+    return render_template('groups.html')
+
+
+
+if __name__ =='__main__':
+	app.run(debug=True)
